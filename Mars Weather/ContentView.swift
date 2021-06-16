310fd126b2e61dacc157d60d4a271d025c0c1cbc
dazzlingeffy <irusya9@mail.ru>
2021-06-16 20:14:28 +0300

    Initial Commit

---
+++ Mars Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mars Weather
+//
+//  Created by ira on 16.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mars Weather/Mars_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mars_WeatherApp.swift
+//  Mars Weather
+//
+//  Created by ira on 16.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Mars_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
